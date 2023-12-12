6c2040bb8cc639eb67bcfbbe9b0c79036c05c10e
JeniaGurman <144277083+JeniaGurman@users.noreply.github.com>
2023-12-12 22:58:56 +0200

    homework 4

--- js/task-2.js
+++ js/task-2.js
@@ -4,7 +4,11 @@ function calcAverageCalories(days) {
     for (const day of days) {
         totalCalories += day.calories;
 
-    }
+  }
+  if (days.length === 0) {
+    return 0;
+  }
+    
     return totalCalories / days.length; 
     
 }
